da48a22cfb2a35c58cf151b8602e8e0443d42e3c
sergei <kvakazuabr@yandex.ru>
2018-03-25 20:20:34 +0300

    19

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+
+
 var link = document.querySelector(".card-link");
 var popup = document.querySelector(".form");
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("form-show");
 });
+
+   
