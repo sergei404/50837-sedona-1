20481e97492f423eb8380960290d6bac00cb1880
sergei <kvakazuabr@yandex.ru>
2018-04-01 21:48:07 +0300

    22.2

--- js/main.js
+++ js/main.js
@@ -33,14 +33,19 @@ var departure = document.querySelector("#departure");*/
      
 var link = document.querySelector(".card-link");
   
-var popup = document.querySelector(".form-modal");
+var popup = document.querySelector(".modal");
 
-var form = document.querySelector("form");
-var arrival = document.querySelector("[name=arrival]");
-var departure = document.querySelector("[name=departure]");
+var form = popap.querySelector("form");
+var arrival = popup.querySelector("[name=arrival]");
+var departure = popup.querySelector("[name=departure]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
+  if (Popup.classList.contains("modal-show")) {
+    Popup.classList.remove("modal-error");
+  }
+
+
   popup.classList.add("form-show");
   arrival.focus();
 });
@@ -49,9 +54,9 @@ link.addEventListener("click", function (evt) {
 form.addEventListener("submit", function (evt) {
   if (!arrival.value || !departure.value) {
     evt.preventDefault();
-    popup.classList.remove("form-error");
+    popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("form-error");
+    popup.classList.add("modal-error");
   } 
 });
 
